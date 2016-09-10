64497408874ce4ae7da02d7fcb5ec4040c1dabef
Joshua Pancho <kvarnsen@users.noreply.github.com>
2016-09-10 16:53:29 +1000

    Added auth and updated routes

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,8 +15,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'first_name' => $faker->firstName,
-        'last_name' => $faker->lastName,
+        'name' => $faker->name,
         'email' => $faker->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
