79819ca0238d9a767ec42e3b9fcbe8f4065bd260
jacquelineshadforth <s3332777@student.rmit.edu.au>
2016-09-10 22:14:26 +1000

    Add CSS styling to global navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:500">
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -26,11 +27,12 @@
     </style>
 </head>
 <body id="app-layout">
+    <!--
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
+                <!-- Collapsed Hamburger
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                     <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
@@ -38,19 +40,55 @@
                     <span class="icon-bar"></span>
                 </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                <!-- Branding Image
+                <a class="navbar-brand navbar-title" href="{ url('/') }">
+                    Mavericks Inc. Cinema
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
+                <!-- Left Side Of Navbar
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{ url('/home') }">Home</a></li>
                 </ul>
 
-                <!-- Right Side Of Navbar -->
+                <!-- Right Side Of Navbar
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links
+                    if (Auth::guest())
+                        <li><a href="{ url('/login') }">Login</a></li>
+                        <li><a href="{ url('/register') }">Register</a></li>
+                    else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                { Auth::user()->name } <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{ url('/logout') }"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+    -->
+
+    <!-- Fixed navigation -->
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top" id="mavericks-nav">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand navbar-title" href="#">Mavericks Inc. Cinema</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
@@ -68,7 +106,7 @@
                         </li>
                     @endif
                 </ul>
-            </div>
+            </div><!--/.nav-collapse -->
         </div>
     </nav>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,10 @@
-@extends('layouts.app')
+<link href="{{ asset('assets/css/site.css') }}" media="all" rel="stylesheet" type="text/css" />
 
+@extends('layouts.app')
 
 @section('content')
 
-    <link href="{{ asset('assets/css/site.css') }}" media="all" rel="stylesheet" type="text/css" />
-
-    <div class="container">
+    <div class="container" id="mavericks-carousel">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
