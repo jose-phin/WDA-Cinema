c1e1da8223b2af8632e09f595b1e61c227c4a746
Joshua Pancho <kvarnsen@users.noreply.github.com>
2016-10-10 12:16:03 +1100

    Added more test and misc refactoring to session time seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
 
         // Create random session times for random movies
         // NOTE: This HAS to be in this position, as it relies on data created in the previous seeders to work
-        factory(App\MovieSession::class, 20)->create();
+        factory(App\MovieSession::class, 30)->create();
 
         // Run our test seeder to create a dummy user and dummy booking
         $this->call(UserTestSeeder::class);
