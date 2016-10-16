157724af993542879b7b4986e0ac9e7cf85b12db
jacquelineshadforth <s3332777@student.rmit.edu.au>
2016-10-16 21:21:55 +1100

    Fix style issues on success page and individual movie page

--- resources/views/booking_success.blade.php
+++ resources/views/booking_success.blade.php
@@ -13,10 +13,15 @@
             @foreach ($bookings as $booking)
                 <div class="success-movie">
                     <p><img src="{{ $booking->session->movie->image_url }}"></p>
-                    <p><strong>{{ $booking->session->movie->title }}</strong></p>
-                    <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $booking->session->location->name }}</p>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> {{ $booking->session->time }}</p>
-                    <p><i class="fa fa-film" aria-hidden="true"></i> Theatre {{ $booking->session->theater }}</p>
+                    <div class="success-movie-info">
+                        <p><strong>{{ $booking->session->movie->title }}</strong></p>
+                        <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $booking->session->location->name }} &nbsp; <i class="fa fa-film" aria-hidden="true"></i> Theatre {{ $booking->session->theater }}</p>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i> {{ $booking->session->time }}</p>
+                        <p><i class="fa fa-ticket" aria-hidden="true"></i> Adults x {{ $booking->adult_qty }}<br>
+                        <i class="fa fa-ticket" aria-hidden="true"></i> Children x {{ $booking->child_qty }}<br>
+                        <i class="fa fa-ticket" aria-hidden="true"></i> Concession x {{ $booking->concession_qty }}</p>
+                        <p><i class="fa fa-money" aria-hidden="true"></i> Subtotal ${{ number_format(($booking->adult_qty * 25) + ($booking->child_qty * 15) + ($booking->concession_qty * 20), 2) }}</p>
+                    </div>
                 </div>
                 <hr class="grey-line">
             @endforeach
--- resources/views/individual_movie.blade.php
+++ resources/views/individual_movie.blade.php
@@ -153,7 +153,7 @@
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="submit" form="cartForm" class="btn btn-primary buy-now" disabled>Add to Cart</button>
+                                <button type="submit" form="cartForm" class="btn btn-primary buy-now redButtonSml" disabled>Add to Cart</button>
                             </div>
                         </div>
                     </div>
