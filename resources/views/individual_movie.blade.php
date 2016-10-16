0bbc8348f32e220f5a521db04fc64a47f04ea459
jacquelineshadforth <s3332777@student.rmit.edu.au>
2016-10-16 14:27:42 +1100

    Move JavaScript to own file

--- resources/views/individual_movie.blade.php
+++ resources/views/individual_movie.blade.php
@@ -171,65 +171,7 @@
         echo "<div id='js' style='display:none'>" . json_encode($output) . "</div>";
         ?>
 
-        <script>
-            $(document).ready(function() {
-                var i, j, k, subtotal;
-                var locations = [];
-                var count = 0;
-                var unique = true;
-                var data = JSON.parse(document.getElementById('js').innerHTML);
-                /* Find unique cinemas */
-                for (i = 0; i < data.location.length; i++) {
-                    for (j = 0; j < locations.length; j++) {
-                        if (locations[j].name == data.location[i].name) {
-                            console.log("Not unique!");
-                            unique = false;
-                        }
-                    }
-                    if (unique) {
-                        locations[count] = {};
-                        locations[count].name = data.location[i].name;
-                        locations[count].id = data.location[i].id;
-                        count++;
-                    }
-                    unique = true;
-                }
-                /* Push to form */
-                for (i = 0; i < locations.length; i++) {
-                    $("#selectLocation").append(new Option(locations[i].name, locations[i].id));
-                }
-                /* Listen for location selection */
-                $("#selectLocation").on("change", function (event) {
-                    for (i = 0; i < data.sessions.length; i++) {
-                        if (data.sessions[i].location_id == $("#selectLocation").val()) {
-                            $("#selectSession").append(new Option(data.sessions[i].time, data.sessions[i].id));
-                        }
-                    }
-                });
-                /* Populate quantities */
-                for (k = 0; k <= 10; k++) {
-                    $(".quantity").append(new Option(k, k));
-                }
-                /* Update subtotal */
-                $(".modal-content").change(function() {
-                    var total = 0.0;
-                    $(".table>tbody>tr").each(function () {
-                        var quantity = $(this).find("option:selected").val();
-                        var price = $(this).find(".price").text().replace(/[^\d.]/, "");
-                        var amount = (quantity * price);
-                        total += parseInt(amount);
-                        $(this).find(".subtotal").text("$" + amount.toFixed(2));
-                    });
-                    $(".total").text("$" + total.toFixed(2));
-                    /* Buy now enabled */
-                    if (total != 0 && $("#selectLocation").val() != null && $("#selectSession").val() != null) {
-                        $(".buy-now").prop("disabled", false);
-                    } else {
-                        $(".buy-now").prop("disabled", true);
-                    }
-                });
-            });
-        </script>
+        <script src="{{ URL::asset('js/bookMovieSession.js') }}"></script>
 
         <script>
             /* Check that a user is logged in - if not, send them to the login screen */
